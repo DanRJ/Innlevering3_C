a86e4bcbf6cb8ee4938f617b3a2a2f66fd9cac8b
danrj <johdan12@nith.no>
2014-11-17 20:05:30 +0100

    Added a method for getting a 2D array

--- tetris.c
+++ tetris.c
@@ -1,10 +1,17 @@
 #include <ncurses.h>
+#include <unistd.h>
+#include <stdlib.h>
+#define DELAY 30000
+#define FIG_SIZE 2
 
+int checkKeyPressed(int ch, char *icon);
+int **getFigure();
+int updateBoard();
 int main()
 {
   WINDOW *tet_win;
-  int ch, height, width, x, y, startx, starty;
-
+  int ch, height, width, x, y, startx, starty, max_x, max_y;
+  char icon = '0';
   height = 20;
   width = 10;
   
@@ -13,36 +20,88 @@ int main()
   startx = 0;
   starty = 0;
   
+  max_x = 0;
+  max_y = 0;
+
+  x = 0;
+  y = 0;
+
   initscr();
   cbreak();
   noecho();
+  // Choose to not have a keypad for tetris window
   keypad(stdscr, TRUE);
+  nodelay(stdscr, TRUE);
   
-  printw("Press F1 to exit");
-  refresh();
-  
-  tet_win = newwin(height, width, starty, startx);
-  keypad(tet_win, TRUE);
+  tet_win = newwin(height, width, (LINES - height) / 2, (COLS - width) / 2);
   box(tet_win, 0, 0);
+
   wrefresh(tet_win);
   refresh();
-  while((ch = wgetch(tet_win)) != KEY_F(1)) 
+  getmaxyx(tet_win, max_y, max_x);
+  int counter = 0;
+  
+  while(1) 
   {
-    switch(ch)
+    
+    if((ch = getch()) == ERR) 
     {
-      case KEY_LEFT:
-        break;
-      case KEY_RIGHT:
-        break;
-      case KEY_UP:
-        break;
-      case KEY_DOWN:
-        break;
+      mvwaddch(tet_win, y, x, icon);
+      wrefresh(tet_win);
+      usleep(DELAY);
+      if(counter == 10)
+      {
+        ++y;
+        counter = 0;
+      }
+      if(y > max_y) 
+      {
+        y = 0;
+        wclear(tet_win);
+        box(tet_win, 0, 0);
+      }
+      counter++;
+    }
+    else 
+    {
+      checkKeyPressed(ch, &icon);
     }
   }
-  delwin(local_win);
+  
+  delwin(tet_win);
 
   endwin();
 
   return 0;
+}
+
+int **getFigure() 
+{
+  int* val = calloc(FIG_SIZE * FIG_SIZE, sizeof(int));
+  int** figure = malloc(FIG_SIZE * sizeof(int));
+  for(int i = 0; i < FIG_SIZE; ++i) 
+  {
+    figure[i] = val + i * FIG_SIZE;
+  }
+  return figure;
+}
+
+int checkKeyPressed(int ch, char *icon) 
+{
+    switch(ch)
+    {
+      case KEY_LEFT:
+        *icon = '1';
+        break;
+      case KEY_RIGHT:
+        *icon = '2';
+        break;
+      case KEY_UP:
+        *icon = '3';
+        break;
+      case KEY_DOWN:
+        *icon = '4';
+        break;
+    }
+    return 1;
 }
